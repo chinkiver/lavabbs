e7faa27b474123c4ccf880b26b0c1921ad047063
陈勇 <chinkiver@sina.com>
2022-02-23 17:35:24 +0800

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,23 +4,13 @@ namespace App\Observers;
 
 use App\Models\Topic;
 
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
-
     public function saving(Topic $topic)
     {
+        // HTMLPurifier 过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
