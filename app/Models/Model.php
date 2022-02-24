b96e7da4a592381c228e9bdaeddd1e7f8fdc4246
陈勇 <chinkiver@sina.com>
2022-02-24 13:46:39 +0800

    回复列表

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 class Model extends BaseModel
 {
     use HasFactory;
+
+    /**
+     * 按 ID 倒序
+     *
+     * @param $query
+     *
+     * @return mixed
+     */
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('id', 'desc');
+    }
 }
