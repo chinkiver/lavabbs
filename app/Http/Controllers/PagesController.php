646d23ba8ac1400d70b9b4aa618ce99bc6e22968
陈勇 <chinkiver@sina.com>
2022-02-25 15:18:29 +0800

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -20,4 +20,15 @@ class PagesController extends Controller
         $title = 'LavaBBS';
         return view('pages.root', compact('title'));
     }
+
+    public function permissionDenied()
+    {
+        // 如果当前用户有权限访问后台，直接跳转访问
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+
+        // 否则使用视图
+        return view('pages.permission_denied');
+    }
 }
