13ce69f06b2f13415d5fa668604f606e292809f0
陈勇 <chinkiver@sina.com>
2022-02-27 11:26:02 +0800

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -4,18 +4,12 @@ namespace App\Observers;
 
 use App\Models\User;
 
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
