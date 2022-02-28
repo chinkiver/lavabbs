e82f3806f033edb77658194d69235961acd634f1
陈勇 <chinkiver@sina.com>
2022-02-28 16:12:49 +0800

    微信登录

--- config/services.php
+++ config/services.php
@@ -36,4 +36,11 @@ return [
         'key' => env('BAIDU_TRANSLATE_KEY'),
     ],
 
+    // 微信测试号
+    'wechat' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
+
 ];
